b377e09f8f430da341179e06a3e030d3e21e7af7
Felipesdr <felipederose@gmail.com>
2024-12-18 14:03:48 -0300

    modify allowed hosts

--- marigil_psicologa/settings.py
+++ marigil_psicologa/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv('DEBUG'))
 
-ALLOWED_HOSTS = ["localhost", "marigilpsicologa.onrender.com"]
+ALLOWED_HOSTS = ["localhost", "marigilpsicologa.onrender.com", "marigil.pythonanywhere.com"]
 
 
 # Application definition
